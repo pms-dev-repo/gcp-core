a0386c275cae49c40696b1b3aa6fce365120d087
pms-dev-repo <huzabeaga@pmsconsulting.cl>
2026-07-25 22:26:13 -0400

    tema

--- components/sidebar.py
+++ components/sidebar.py
@@ -22,7 +22,7 @@ SHOW_SEARCH = False
 
 PRIMARY_ITEMS = [
     ("▣", "Dashboard", "dashboard", SHOW_DASHBOARD),
-    ("✉", "Guest Mailing", "Guest Mailing", True),
+    ("✉", "Guest Mailing", "communications", True),
     ("▤", "Templates", "templates", SHOW_TEMPLATES),
     ("◷", "Communication History", "history", SHOW_HISTORY),
 ]
